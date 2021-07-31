41b77fea87dea6fe8756884d504f28d9df336021
woofymax <64120270+woofymax@users.noreply.github.com>
2021-07-31 20:40:56 +1000

    made nice io colors bold

--- src/nio.rs
+++ src/nio.rs
@@ -1,9 +1,9 @@
 use ansi_term::Colour;
 
 pub fn red(text: String) {
-    println!("{}", Colour::Red.paint(text));
+    println!("{}", Colour::Red.bold().paint(text));
 }   
 
 pub fn green(text: String) {
-    println!("{}", Colour::Green.paint(text))
+    println!("{}", Colour::Green.bold().paint(text))
 }   
