88f06e235d5664fc5afd3bbd4be81e36ea1853da
woofymax <wolfencrime@gmail.com>
2021-08-01 12:57:07 +1000

    split custom flag operations into their own functions

--- src/codegen.rs
+++ src/codegen.rs
@@ -69,65 +69,93 @@ fn enumerate_custom_flags(
         if custom_flag.replace_with.to_lowercase().starts_with("arg|")
             && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
         {
-            let index = custom_flag
-                .replace_with
-                .replacen("arg|", "", 1)
-                .parse::<usize>()
-                .unwrap();
-            if index > args.len() {
-                red("Insufficient command line arguments for custom flag".to_string());
-                panic!()
-            }
-            source = source.replace(
-                format!("%!%{}%!%", custom_flag.name).as_str(),
-                args[index - 1].as_str(),
-            )
+            source = arg(custom_flag, args, source);
         } else if custom_flag.replace_with.to_lowercase().starts_with("str|")
             && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
         {
-            source = source.replace(
-                format!("%!%{}%!%", custom_flag.name).as_str(),
-                custom_flag
-                    .replace_with
-                    .to_lowercase()
-                    .replacen("str|", "", 1)
-                    .as_str(),
-            );
+            source = str(custom_flag, source)
         } else if custom_flag.replace_with.to_lowercase().starts_with("file|")
             && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
         {
-            let path = custom_flag
-                .replace_with
-                .to_lowercase()
-                .replacen("file|", "", 1);
-            let file = read_flag_file(path);
-            source = source.replace(
-                format!("%!%{}%!%", custom_flag.name).as_str(),
-                file.as_str(),
-            );
+            source = file(custom_flag, source)
         } else if custom_flag
             .replace_with
             .to_lowercase()
             .starts_with("argfile|")
             && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
         {
-            let index = custom_flag
-                .replace_with
-                .replacen("argfile|", "", 1)
-                .parse::<usize>()
-                .unwrap();
-            println!("{}", args[0]);
-            if index > args.len() {
-                red("Insufficient command line arguments for custom flag".to_string());
-                panic!();
-            }
-            let path = args[index - 1].clone();
-            let file = read_flag_file(path);
-            source = source.replace(
-                format!("%!%{}%!%", custom_flag.name).as_str(),
-                file.as_str(),
-            );
+            source = argfile(custom_flag, args, source)
         }
     }
     source
+}
+
+/*******************************************************************************
+*****Replace a custom flag with text specified in a positional CLI argument*****
+*******************************************************************************/
+
+fn arg(flag: CustomFlag, args: &[String], source: String) -> String {
+    let index = remove_prefix(flag.replace_with, &"arg|")
+        .parse::<usize>()
+        .unwrap();
+
+    check_args(args, index);
+    source.replace(
+        format!("%!%{}%!%", flag.name).as_str(),
+        args[index - 1].as_str(),
+    )
+}
+
+/*********************************************************************************
+*****Replace a custom flag with a file specified in a positional CLI argument*****
+*********************************************************************************/
+
+fn argfile(flag: CustomFlag, args: &[String], source: String) -> String {
+    let index = remove_prefix(flag.replace_with, &"argfile|")
+        .parse::<usize>()
+        .unwrap();
+    check_args(args, index);
+    let path = args[index - 1].clone();
+    let file = read_flag_file(path);
+    source.replace(format!("%!%{}%!%", flag.name).as_str(), file.as_str())
+}
+
+/******************************************
+*****Replace a custom flag with a file*****
+******************************************/
+
+fn file(flag: CustomFlag, source: String) -> String {
+    let path = remove_prefix(flag.replace_with, &"file|");
+    let file = read_flag_file(path);
+    source.replace(format!("%!%{}%!%", flag.name).as_str(), file.as_str())
+}
+
+/*
+*****Replace a custom flag with a string*****
+*/
+
+fn str(flag: CustomFlag, source: String) -> String {
+    source.replace(
+        format!("%!%{}%!%", flag.name).as_str(),
+        remove_prefix(flag.replace_with, &"str|").as_str(),
+    )
+}
+
+/*************************************************
+*****Check if sufficient arguments are passed*****
+*************************************************/
+
+fn check_args(args: &[String], index: usize) {
+    if index > args.len() {
+        red("Insufficient command line arguments for custom flag".to_string());
+        panic!()
+    }
+}
+
+/********************************************
+*****Remove the prefix from replace_with*****
+********************************************/
+
+fn remove_prefix(replace_with: String, prefix: &&str) -> String {
+    replace_with.to_lowercase().replacen(prefix, "", 1)
 }
