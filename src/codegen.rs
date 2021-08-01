aff16a5feb6b34cd9e5730c2d8ebc85ecfa60985
woofymax <64120270+woofymax@users.noreply.github.com>
2021-08-01 10:31:36 +1000

    moved the custom flag enumeration loop out of the main codegen functin

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,6 +1,6 @@
 use crate::file_rw::read_flag_file;
 use crate::nio::*;
-use crate::read_config::ConfigFile;
+use crate::read_config::{ConfigFile, CustomFlag};
 
 /*************************************************
 *****Generate the output code for a blueprint*****
@@ -39,79 +39,78 @@ pub fn codegen(
         );
 
         if let Some(customs) = flags.custom_flags {
-            for custom_flag in customs {
-                if custom_flag.replace_with.to_lowercase().starts_with("arg|")
-                    && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
-                {
-                    let index = custom_flag
-                        .replace_with
-                        .replacen("arg|", "", 1)
-                        .parse::<usize>()
-                        .unwrap();
-                    if index > args.len() {
-                        red("Insufficient command line arguments for custom flag".to_string());
-                        panic!()
-                    }
-                    source = source.replace(
-                        format!("%!%{}%!%", custom_flag.name).as_str(),
-                        args[index - 1].as_str(),
-                    )
-                } else if custom_flag.replace_with.to_lowercase().starts_with("str|")
-                    && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
-                {
-                    source = source.replace(
-                        format!("%!%{}%!%", custom_flag.name).as_str(),
-                        custom_flag
-                            .replace_with
-                            .to_lowercase()
-                            .replacen("str|", "", 1)
-                            .as_str(),
-                    );
-                } else if custom_flag.replace_with.to_lowercase().starts_with("file|")
-                    && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
-                {
-                    let path = custom_flag
-                        .replace_with
-                        .to_lowercase()
-                        .replacen("file|", "", 1);
-                    let file = read_flag_file(path);
-                    source = source.replace(
-                        format!("%!%{}%!%", custom_flag.name).as_str(),
-                        file.as_str(),
-                    );
-                } else if custom_flag
-                    .replace_with
-                    .to_lowercase()
-                    .starts_with("argfile|")
-                    && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
-                {
-                    let index = custom_flag
-                        .replace_with
-                        .replacen("argfile|", "", 1)
-                        .parse::<usize>()
-                        .unwrap();
-                    println!("{}", args[0]);
-                    if index > args.len() {
-                        red("Insufficient command line arguments for custom flag".to_string());
-                        panic!();
-                    }
-                    let path = args[index - 1].clone();
-                    let file = read_flag_file(path);
-                    source = source.replace(
-                        format!("%!%{}%!%", custom_flag.name).as_str(),
-                        file.as_str(),
-                    );
-                }
-            }
+            source = enumerate_custom_flags(source, customs, args)
         }
         return source;
     }
-    source = source.replace("%!%ModuleName%!%", module_name.as_str());
-    if let Some(license) = options.project.license {
-        source = source.replace("%!%License%!%", read_flag_file(license).as_str());
-    };
-    if let Some(version) = options.project.version {
-        source = source.replace("%!%Version%!%", version.as_str());
-    };
+    source
+}
+
+fn enumerate_custom_flags(src: String, customs: std::vec::Vec<CustomFlag>, args: &[String]) -> String {
+    let mut source = src;
+    for custom_flag in customs {
+        if custom_flag.replace_with.to_lowercase().starts_with("arg|")
+            && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
+        {
+            let index = custom_flag
+                .replace_with
+                .replacen("arg|", "", 1)
+                .parse::<usize>()
+                .unwrap();
+            if index > args.len() {
+                red("Insufficient command line arguments for custom flag".to_string());
+                panic!()
+            }
+            source = source.replace(
+                format!("%!%{}%!%", custom_flag.name).as_str(),
+                args[index - 1].as_str(),
+            )
+        } else if custom_flag.replace_with.to_lowercase().starts_with("str|")
+            && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
+        {
+            source = source.replace(
+                format!("%!%{}%!%", custom_flag.name).as_str(),
+                custom_flag
+                    .replace_with
+                    .to_lowercase()
+                    .replacen("str|", "", 1)
+                    .as_str(),
+            );
+        } else if custom_flag.replace_with.to_lowercase().starts_with("file|")
+            && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
+        {
+            let path = custom_flag
+                .replace_with
+                .to_lowercase()
+                .replacen("file|", "", 1);
+            let file = read_flag_file(path);
+            source = source.replace(
+                format!("%!%{}%!%", custom_flag.name).as_str(),
+                file.as_str(),
+            );
+        } else if custom_flag
+            .replace_with
+            .to_lowercase()
+            .starts_with("argfile|")
+            && source.contains(format!("%!%{}%!%", custom_flag.name).as_str())
+        {
+            let index = custom_flag
+                .replace_with
+                .replacen("argfile|", "", 1)
+                .parse::<usize>()
+                .unwrap();
+            println!("{}", args[0]);
+            if index > args.len() {
+                red("Insufficient command line arguments for custom flag".to_string());
+                panic!();
+            }
+            let path = args[index - 1].clone();
+            let file = read_flag_file(path);
+            source = source.replace(
+                format!("%!%{}%!%", custom_flag.name).as_str(),
+                file.as_str(),
+            );
+        }
+    }
     source
 }
